2ad23c1a1b550dda30a4a5d0128fb29ef89f7869
kinakomoch7 <book-insect14@outlook.jp>
2023-03-17 16:53:25 +0900

    コミットするの忘れていてreactの使用を断念して、firebaseだけでも使おうとしたが扱いきれなかった変更です。

---
+++ public/index.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+  
+const firebaseConfig = {
+  apiKey: "AIzaSyBoqEmnSVpN-EbQSUlNn3NB9gadVLitq3U",
+  authDomain: "expenseapp-d6cb0.firebaseapp.com",
+  projectId: "expenseapp-d6cb0",
+  storageBucket: "expenseapp-d6cb0.appspot.com",
+  messagingSenderId: "825294800053",
+  appId: "1:825294800053:web:db70d5566a30cc6365aafc",
+  measurementId: "G-2W7P9WVN26"
+};
+
+firebase.initializeApp(config);
